7ccbd7561305a60fa5897124f4f541c07cd78043
RANGESHPANDIAN PT <rangeshpandian@gmail.com>
2025-09-04 15:04:12 +0530

    Add files via upload

---
+++ HackerRank/Arithmetic Operators.js
@@ -0,0 +1,33 @@
+//Day 1: Arithmetic Operators
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+function getArea(length, width) {
+    let area = length * width;   
+    return area;
+}
+function getPerimeter(length, width) {
+    let perimeter = 2 * (length + width);   
+    return perimeter;
+}
