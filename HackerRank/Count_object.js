23e232a1808a417c401edcb337a660dac2828d9e
RANGESHPANDIAN PT <rangeshpandian@gmail.com>
2025-09-14 00:47:29 +0530

    Add files via upload

---
+++ HackerRank/Count_object.js
@@ -0,0 +1,60 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function getCount(objects) {
+    let count = 0;
+    for (let obj of objects) {
+        if (obj.x === obj.y) {
+            count++;
+        }
+    }
+    return count;
+}
+
+function main() {
+    const n = parseInt(readLine());   // number of objects
+    let objects = [];
+    
+    for (let i = 0; i < n; i++) {
+        const [a, b] = readLine().split(' ').map(Number);
+        objects.push({ x: a, y: b });
+    }
+    
+    console.log(getCount(objects));
+}
+
+
+
+function main() {
+    const n = +(readLine());
+    let objects = [];
+    
+    for (let i = 0; i < n; i++) {
+        const [a, b] = readLine().split(' ');
+        
+        objects.push({x: +(a), y: +(b)});
+    }
+    
+    console.log(getCount(objects));
+}
