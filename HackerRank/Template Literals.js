5535e195dcaa4230487c37c58a58b3f2bc2417ae
RANGESHPANDIAN PT <rangeshpandian@gmail.com>
2025-09-18 19:22:38 +0530

    Add files via upload

---
+++ HackerRank/Template Literals.js
@@ -0,0 +1,47 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function sides(literals, ...expressions) {
+    let A = expressions[0];  
+    let P = expressions[1];  
+    
+    let root = Math.sqrt((P * P) - (16 * A));
+    let s1 = (P - root) / 4;
+    let s2 = (P + root) / 4;
+    
+    return [s1, s2].sort((a, b) => a - b);
+}
+
+
+function main() {
+    let s1 = +(readLine());
+    let s2 = +(readLine());
+    
+    [s1, s2] = [s1, s2].sort();
+    
+    const [x, y] = sides`The area is: ${s1 * s2}.\nThe perimeter is: ${2 * (s1 + s2)}.`;
+    
+    console.log((s1 === x) ? s1 : -1);
+    console.log((s2 === y) ? s2 : -1);
+}
